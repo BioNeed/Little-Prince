a96f731fad8a1d407faa90be7f11fc10c6477159
BioNeed_X <cianis2002@gmail.com>
2023-04-15 21:23:11 +0300

    Added respawn and death

---
+++ Assets/Scripts/Respawn/DeathZone.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour
+{
+    [SerializeField] private Transform _respawnPoint;
+    [SerializeField] private GameObject _deathScreen;
+
+    private Player _deadPlayer;
+    private bool _isDead;
+
+    private void Start()
+    {
+        _deathScreen.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (_isDead == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                RespawnPlayer();
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out Player player))
+        {
+            _deadPlayer = player;
+            PauseGame();  
+        }
+    }
+
+    private void PauseGame()
+    {
+        Time.timeScale = 0;
+        _isDead = true;
+        _deathScreen.SetActive(true);
+    }
+
+    private void RespawnPlayer()
+    {
+        _deadPlayer.transform.position = _respawnPoint.position;
+        Time.timeScale = 1;
+        _isDead = false;
+        _deathScreen.SetActive(false);
+    }
+}
