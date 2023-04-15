d8a69779164d63aa1a990d7a27875c61fbb0b145
BioNeed_X <cianis2002@gmail.com>
2023-04-15 22:18:11 +0300

    Fixed BranchFinder

--- Assets/Scripts/Branch&Pulling/BranchFinder.cs
+++ Assets/Scripts/Branch&Pulling/BranchFinder.cs
@@ -5,20 +5,18 @@ public class BranchFinder : MonoBehaviour
     [SerializeField] private Transform _target;
     [SerializeField] private PhysicsMovement _physicsMovement;
     [SerializeField] private VerticalMovement _verticalMovement;
+    [SerializeField] private LayerMask _layerMask;
 
     public void FindBranchToPull()
     {
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        RaycastHit2D[] hits = Physics2D.RaycastAll(mousePosition, mousePosition);
-        for (int i = 0; i < hits.Length; i++)
+        RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero, Mathf.Infinity, _layerMask);
+
+        if (hit.collider != null && hit.collider.TryGetComponent(out BranchPulling branchPulling))
         {
-            if (hits[i].collider.TryGetComponent(out BranchPulling branchPulling))
-            {
-                branchPulling.TryStartPulling(_target);
-                _verticalMovement.StopJump();
-                _physicsMovement.SetBranchPulling(branchPulling);
-                break;
-            }
+            branchPulling.TryStartPulling(_target);
+            _verticalMovement.StopJump();
+            _physicsMovement.SetBranchPulling(branchPulling);
         }
     }
 }
