30d97f2f9c32bda41fdc4f711c2e1781c7da7c33
BioNeed_X <cianis2002@gmail.com>
2024-02-25 16:56:27 +0100

    Bells are prepared

--- Assets/Scripts/BellPuzzle/BellCollision.cs
+++ Assets/Scripts/BellPuzzle/BellCollision.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 public class BellCollision : MonoBehaviour
 {
     [SerializeField] private Bell _bell;
+    [SerializeField] private Animator _bellAnimator;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.GetComponent<Player>() != null)
         {
             _bell.RingBell();
+            _bellAnimator.SetTrigger("IsRinging");
         }
     }
 }
